06f77fa6c703520c3e7d14e647231038b87ffde0
wenmingyue <664461395@qq.com>
2025-04-24 08:54:48 +0800

    build: pack 20250424

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'mapweb'
-    : '/',
-  outputDir: 'dist',
   productionSourceMap: false,
   css: {
     loaderOptions: {
